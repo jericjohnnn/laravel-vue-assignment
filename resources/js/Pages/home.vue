dd0689a6faf37fd98ff03b86e67541616fb7f92e
JJA <jericjohnaligato@gmail.com>
2024-07-05 14:49:57 +0800

    laravel-vue assignment first commit

---
+++ resources/js/Pages/home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <div class="flex items-center">
+    <button @click="count++" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+      Count is:
+    </button>
+    <span class="text-black font-bold">{{ count }}</span>
+  </div>
+</template>
