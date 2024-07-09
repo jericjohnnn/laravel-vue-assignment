a1b5c56be8f7fa518b52ad79e2f7304af72ee9c5
JJA <jericjohnaligato@gmail.com>
2024-07-09 18:58:55 +0800

    update

--- resources/js/Sections/hero.vue
+++ resources/js/Sections/hero.vue
@@ -4,41 +4,44 @@ import CTAbutton from '../Components/CTAbutton.vue';
 
 </script>
 <template>
-  <section class="pt-36 pb-64 md:pt-64 md:pb-72 lg:pb-64 xl:pb-72 bg-herobg bg-cover bg-top relative overflow-hidden" id="hero">
-      
-      <div class="container mx-auto px-4 lg:px-10 ">
-        <div class="flex flex-wrap items-center">
+  <section class="pt-36 pb-64 md:pt-64 md:pb-72 lg:pb-64 xl:pb-72 bg-herobg bg-cover bg-top relative overflow-hidden"
+    id="hero">
+
+    <div class="container mx-auto px-4 lg:px-10 ">
+      <div class="flex flex-wrap items-center">
         <!-- TEXTS -->
-          <div v-motion-slide-visible-once-left class="w-full md:w-1/2 flex flex-col items-center justify-center md:items-start ">
-            <h1 class="font-bold text-5xl leading-tight lg:leading-snug xl:text-6xl xl:leading-snug lg:pr-30 mb-4 font-montserrat text-center md:text-start
+        <div v-motion-slide-visible-once-left
+          class="w-full md:w-1/2 flex flex-col items-center justify-center md:items-start ">
+          <h1 class="font-bold text-5xl leading-tight lg:leading-snug xl:text-6xl xl:leading-snug lg:pr-30 mb-4 font-montserrat text-center md:text-start
             bg-clip-text text-transparent bg-gradient-to-t from-hcolorbot to-hcolortop">
-                Your Idea, Our Expertise</h1>
-            <p class="text-black mb-6 text-center font-hind text-lg xl:text-xl xl:leading-loose leading-relaxed lg:leading-9 md:text-start">
-                From Web and Mobile Development to Seamless Project Management, 
-                We Help Your Ideas Ascend and Achieve Results.</p>
-            <CTAbutton>Let's Talk</CTAbutton>
-          </div>
+            Your Idea, Our Expertise</h1>
+          <p
+            class="text-black mb-6 text-center font-hind text-lg xl:text-xl xl:leading-loose leading-relaxed lg:leading-9 md:text-start">
+            From Web and Mobile Development to Seamless Project Management,
+            We Help Your Ideas Ascend and Achieve Results.</p>
+          <CTAbutton>Let's Talk</CTAbutton>
+        </div>
         <!-- IMAGE -->
-          <div v-motion-slide-visible-once-right class="w-full md:w-1/2 mt-24 sm:mt-36 md:mt-0 z-50">
-            <div class="h-64  flex items-center justify-center
+        <div v-motion-slide-visible-once-right class="w-full md:w-1/2 mt-24 sm:mt-36 md:mt-0 z-50">
+          <div class="h-64  flex items-center justify-center
             md:justify-end">
-                <img class="sm:w-10/12 md:w-96 lg:w-11/12 z-50" src="../../assets/images/heroimg.svg" alt="">
-            </div>
+            <img class="sm:w-10/12 md:w-96 lg:w-11/12 z-50" src="../../assets/images/heroimg.svg" alt="">
           </div>
+        </div>
 
-        </div>
       </div>
-      <div class=" hidden lg:block absolute inset-0 pointer-events-none overflow-hidden z-0">
-        <div class="absolute right-0 animate-slide z-0">
-          <img class="h-72 z-0" src="../../assets/images/servicescloud2.svg" alt="">
-        </div>
+    </div>
+    <div class=" hidden lg:block absolute inset-0 pointer-events-none overflow-hidden z-0">
+      <div class="absolute right-0 animate-slide z-0">
+        <img class="h-72 z-0" src="../../assets/images/servicescloud2.svg" alt="">
       </div>
-      <div class="hidden lg:block absolute inset-0 pointer-events-none overflow-hidden">
-        <div class="absolute left-0 bottom-10 animate-slide-right z-20">
-          <img class="h-72 z-20" src="../../assets/images/servicescloud2.svg" alt="">
-        </div>
+    </div>
+    <div class="hidden lg:block absolute inset-0 pointer-events-none overflow-hidden">
+      <div class="absolute left-0 bottom-10 animate-slide-right z-20">
+        <img class="h-72 z-20" src="../../assets/images/servicescloud2.svg" alt="">
       </div>
+    </div>
 
-    </section>
+  </section>
 
 </template>
--- resources/js/Sections/services.vue
+++ resources/js/Sections/services.vue
@@ -14,7 +14,8 @@
 
       <div class="container mx-auto px-4 lg:px-10">
         <div class="flex px-8 md:px-0 flex-wrap-reverse justify-center">
-          <div v-motion-pop-visible-once class="w-full md:w-1/2 relative flex justify-center lg:justify-start lg:items-center">
+          <div v-motion-pop-visible-once
+            class="w-full md:w-1/2 relative flex justify-center lg:justify-start lg:items-center">
             <div class="flex justify-center lg:justify-start ">
               <img class="sm:w-5/6 md:w-full lg:w-5/6" src="../../assets/images/team.svg" alt="">
             </div>
@@ -32,14 +33,14 @@
             class="w-full mb-10 md:mb-0 mt-16 md:w-1/2 pl-0 md:pl-8 md:mt-0 text-justify leading-relaxed font-hind ">
             <div class="text-center md:text-start  mb-10">
               <div class="relative">
-              <h2 class="text-4xl xl:text-5xl font-bold font-montserrat  
+                <h2 class="text-4xl xl:text-5xl font-bold font-montserrat  
                   bg-clip-text text-transparent bg-gradient-to-t from-hcolorbot to-hcolortop">
-                What We Do
-              </h2>
-              <div class=" absolute -right-14 bottom-10 sm:right-5 sm:bottom-3 md:right-0 lg:right-20 xl:right-36">
-                <img class="w-4/6 lg:w-5/6 xl:w-full" src="../../assets/images/ascend.svg" alt="">
+                  What We Do
+                </h2>
+                <div class=" absolute -right-14 bottom-10 sm:right-5 sm:bottom-3 md:right-0 lg:right-20 xl:right-36">
+                  <img class="w-4/6 lg:w-5/6 xl:w-full" src="../../assets/images/ascend.svg" alt="">
+                </div>
               </div>
-            </div>
               <div class="flex justify-center md:justify-start mt-2 ">
                 <div class="h-1 w-16 xl:w-20 bg-blue rounded"></div> <!-- UNDERLINE -->
               </div>
@@ -87,9 +88,10 @@
             <div class="flex items-center justify-center">
               <img class="md:h-40" src="../../assets/images/webdev.svg" alt="">
             </div>
-            <h3 class="text-2xl text-center font-bold mb-5 mt-10 font-montserrat text-black lg:mt-10">Web Development</h3>
+            <h3 class="text-2xl text-center font-bold mb-5 mt-10 font-montserrat text-black lg:mt-10">Web Development
+            </h3>
             <p class="font-hind text-gray-500 leading-relaxed md:px-0 lg:px-8  xl:text-lg xl:leading-loose">
-              We create stunning websites with HTML5, CSS3, JavaScript, React, and Angular. Our services 
+              We create stunning websites with HTML5, CSS3, JavaScript, React, and Angular. Our services
               include responsive design, API integrations, and robust backend solutions.</p>
           </div>
 
@@ -99,7 +101,9 @@
               <img class="md:h-40" src="../../assets/images/mobiledev.svg" alt="">
             </div>
             <h3 class="text-2xl text-center font-bold mb-5 mt-10 font-montserrat text-black">Mobile Development</h3>
-            <p class="font-hind text-gray-500 leading-relaxed md:px-0 lg:px-8  xl:text-lg xl:leading-loose">We specialize in native and cross-platform mobile app development using Swift, Kotlin, and Flutter. Our services are tailored to meet your specific needs with excellence.</p>
+            <p class="font-hind text-gray-500 leading-relaxed md:px-0 lg:px-8  xl:text-lg xl:leading-loose">We
+              specialize in native and cross-platform mobile app development using Swift, Kotlin, and Flutter. Our
+              services are tailored to meet your specific needs with excellence.</p>
           </div>
 
           <!-- PROJMA -->
@@ -108,7 +112,9 @@
               <img class="md:h-40" src="../../assets/images/projmanager.svg" alt="">
             </div>
             <h3 class="text-2xl text-center font-bold mb-5 mt-10 font-montserrat text-black">Project Management</h3>
-            <p class="font-hind text-gray-500 leading-relaxed md:px-0 lg:px-8  xl:text-lg xl:leading-loose">We offer expert project management to ensure initiative success. Our approach focuses on clear communication, efficient resource use, and timely delivery.</p>
+            <p class="font-hind text-gray-500 leading-relaxed md:px-0 lg:px-8  xl:text-lg xl:leading-loose">We offer
+              expert project management to ensure initiative success. Our approach focuses on clear communication,
+              efficient resource use, and timely delivery.</p>
           </div>
 
         </div>
